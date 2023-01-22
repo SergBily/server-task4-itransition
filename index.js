586e1339f33d59e446955d38b3a90566917fb28d
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-22 16:33:11 +0300

    fix: environment

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use(cors({
   origin: [process.env.CLIENT_URL],
   methods: ["GET", "POST", "DELETE", "PATCH"],
